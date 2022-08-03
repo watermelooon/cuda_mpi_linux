526f82ae532b5901b44915cfa316bccb4b9ebb9f
Shawn <40096719+watermelooon@users.noreply.github.com>
2022-08-03 12:51:42 +0800

    Add files via upload

---
+++ cuda_mpi_linux/sample1/simplempi.h
@@ -0,0 +1,7 @@
+// Forward declarations
+extern "C" {
+    void initData(float *data, int dataSize);
+    void computeGPU(float *hostData, int blockSize, int gridSize);
+    float sum(float *data, int size);
+    void my_abort(int err);
+}
---
+++ cuda_mpi_linux/sample2/test.h
@@ -0,0 +1,4 @@
+extern "C"
+{
+	void cudaFun ( int is , FILE  *fp ,  int  nx , int nz );
+}
